3f96a1ee89c7f0cf383ca13fd1d19883a806e549
VeneraTangalycheva <verena318sh@gmail.com>
2019-11-27 00:58:42 +0300

    weight perfect1

---
+++ weight_perfect.rb
@@ -0,0 +1,15 @@
+class WeightPerfect
+	puts "Введите ваше имя?"
+	name = gets.chomp
+
+	puts "Укажите ваш рост?"
+	tall = gets.chomp
+
+	weight_perfect = (tall.to_i - 110.0)*1.15
+	
+	if weight_perfect < 0 
+	puts "#{name}, Ваш вес уже оптимальный"
+	else
+	puts "#{name}, Ваш вес идеальный"
+	end
+end
